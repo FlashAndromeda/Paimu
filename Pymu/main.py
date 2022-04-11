4529feb4036690616df932d5c7184ec6ee7bb394
FlashDelirium <michalfleites@gmail.com>
2022-04-11 12:59:47 +0200

    testing

--- Pymu/main.py
+++ Pymu/main.py
@@ -1,57 +1,30 @@
 import os
-import sys
 import random
-import discord
 from dotenv import load_dotenv
+
+# 1
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+# 2
+bot = commands.Bot(command_prefix='...')
 
-# Paimu hello
-@client.event
+@bot.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'  {guild.name} (id: {guild.id})'
-    )
+    print(f'{bot.user.name} has connected to discord!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command(name='hello', help="Returns the user's greeting.")
+async def hello(ctx):
+    await ctx.send(f'Hello!')
 
-    content = message.content.lower()
-    author_name = str(message.author)[0:-5]
+@bot.command(name='diceroll', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(f"Your roll: {', '.join(dice)}")
 
-    greetingslist = ['Hey ', 'Hello ', 'Heyoo ', 'Yo ', 'Wassup ', 'Howdy ', 'Greetings ', 'Sup ', 'Nice seeing you ', "It's good to see you "]
-    goodbyelist = ['Cya ', 'See ya ', 'See you later ', 'Bye ', 'Goodbye ', 'See you around ']
-
-    if 'paimu' and 'help' in content:
-        with open('help.txt') as file:
-            for line in file:
-                await message.channel.send(line)
-    elif message.content == 'raise-exception':
-        raise discord.DiscordException
-
-    if 'paimu' and 'hello' in content:
-        greeting = random.choice(greetingslist)
-        await message.channel.send(f'{greeting}{author_name}')
-
-    if 'paimu' and 'bye' in content:
-        goodbye = random.choice(goodbyelist)
-        await message.channel.send(f'{goodbye}{author_name}')
-
-@client.event
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n'
-                    f'{sys.exc_info()}\n')
-        else:
-            raise
-
-client.run(TOKEN)
+bot.run(TOKEN)
